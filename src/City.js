f793e6441a4028b0a01aa83dd5d32d3b089d4ea4
Nishit <nishitdjoshi@gmail.com>
2020-11-11 18:52:46 -0600

    react hooks

--- src/City.js
+++ src/City.js
@@ -3,9 +3,10 @@ import React from "react";
 export default function City({ state, city, weather }) {
   return (
     <div>
-      <h1>StateName: {state}</h1>
+      <h2>StateName: {state}</h2>
       <h2>{city}</h2>
       <h2>{weather}</h2>
+      <hr />
     </div>
   );
 }
